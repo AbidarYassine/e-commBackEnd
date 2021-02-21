1e173f0aad10de20fbb45eed5e6f5fd7bcd6784a
naj1335 <79324182+naj1335@users.noreply.github.com>
2021-02-21 16:30:00 +0100

    Add files via upload

---
+++ 2021_02_19_170021_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ertdesignation');
+            $table->float('prix');
+            $table->integer('qtestock');
+            $table->integer('tauttva');
+            $table->integer('tautremise');
+            $table->string('artimg');
+            $table->string('artdescription');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
